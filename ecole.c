2daf02e8e3e5b110c8bd6b23c234832949e185c6
Goldwasser <fumumue@gmail.com>
2020-01-01 20:00:33 +0900

    modified

--- ecole.c
+++ ecole.c
@@ -4,10 +4,11 @@
 
 #include <stdio.h>
 
-#define O 256
+#define O 8
 
 /* generate Galois Field over GF(2^?) */
-static const char *normal[13]={
+static const char *normal[14]={
+  "1101",
   "11001", /* GF(16) */
   "110111",
   "1100001",
@@ -52,58 +53,64 @@ void mkgf(int n)
 
   /* define pol */
   switch(n){
-  case 16:
+
+  case 8:
     pol=strtoul(normal[0],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
-  case 32:
+  case 16:
     pol=strtoul(normal[1],(char **)NULL,2);
+    printf("%d\n",n);
+    break;
+
+  case 32:
+    pol=strtoul(normal[2],(char **)NULL,2);
 printf("%d\n",n);
     break;
 
   case 64:
-    pol=strtoul(normal[2],(char **)NULL,2);
+    pol=strtoul(normal[3],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
   case 128:
-   pol=strtoul(normal[3],(char **)NULL,2);
+   pol=strtoul(normal[4],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
   case 256:
-    pol=strtoul(normal[4],(char **)NULL,2);
-    printf("%d\n",n);
-    break;
-
-  case 512:
     pol=strtoul(normal[5],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
-  case 1024:
+  case 512:
     pol=strtoul(normal[6],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
-  case 2048:
+  case 1024:
     pol=strtoul(normal[7],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
-  case 4096:
+  case 2048:
     pol=strtoul(normal[8],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
+  case 4096:
+    pol=strtoul(normal[9],(char **)NULL,2);
+    printf("%d\n",n);
+    break;
+
   case 8192:
-   pol=strtoul(normal[9],(char **)NULL,2);
+   pol=strtoul(normal[10],(char **)NULL,2);
     printf("%d\n",n);
     break;
 
   default : /* 16384 */
-    pol=strtoul(normal[10],(char **)NULL,2);
+    pol=strtoul(normal[11],(char **)NULL,2);
     printf("%d\n",n);
     break;
   }
@@ -138,9 +145,17 @@ printf("%d\n",n);
 
 }
 
-/*
+
 int main(){
-  int n;
-  makegf(512);
+  int i,j,k;
+
+  mkgf(O);
+  makefg(O);
+  printf("fg[%d]={",O);
+  for(i=0;i<O;i++)
+  printf("%d,",fg[i]);
+printf("};\n");
+
+
+return 0;
 }
-*/
